e0e9f4beba020adf27e0166447043505ffbd3ed6
Olaide E.O <ebenezerie@yahoo.com>
2021-07-06 15:10:33 +0100

    add frontend interview data

---
+++ Interview/front-end/frontendDirTree.js
@@ -0,0 +1,38 @@
+const fs = require('fs');
+
+const dirTree = require("./frontendDirectoryConverter");
+const flatten = require('tree-flatten');
+const filteredTree = dirTree("./frontend-master", { extensions: /\.(md|js|py|html)$/, exclude: /__init__\.py/ });
+
+const flattenTree =   flatten(filteredTree, 'children')
+
+//console.dir(filteredTree, { depth: null});
+
+//console.dir(flattenTree, { depth: null});
+
+const length = flattenTree.length
+
+for ( let i = 0; i < length - 1; i++){
+	flattenTree[ i + 1].index = i
+}
+
+for(let i = 0; i < length - 1; i++){
+  flattenTree[ i + 1 ].title	= flattenTree[ i + 1 ].title.toLowerCase()
+    .split('_')
+    .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+    .join(' ')
+}
+
+for(let i = 0; i < length - 1; i++){
+    flattenTree[ i + 1 ].title	= flattenTree[ i + 1 ].title.toLowerCase()
+      .split('-')
+      .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+      .join(' ')
+  }
+
+fs.writeFile("./frontendDataMain.js", JSON.stringify(flattenTree), function(err) {
+    if(err) {
+        return console.log(err);
+    }
+    console.log("The file was saved!");
+}); 
